7195919300c09ba767e89e1940c5ba16723a8519
Ujjwal Raizada <ujjwalraizada@gmail.com>
2020-01-08 01:47:43 +0530

    Add auth endpoints

---
+++ onlineCAL/booking_portal/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookingPortalConfig(AppConfig):
+    name = 'booking_portal'
---
+++ onlineCAL/booking_portal/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+    }
+    return render(request, 'home.html', context=context)
