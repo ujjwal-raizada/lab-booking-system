ff74b4b020c9c7e57c3b007cc745631d5bc5b699
Daksh <dyashlaha039909@gmail.com>
2020-01-25 16:08:37 +0530

    Django: Add FESEM form

--- server/booking_portal/urls.py
+++ server/booking_portal/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book-machine', views.book_machine, name='book-machine')
 ]
--- server/booking_portal/views.py
+++ server/booking_portal/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import FESEMForm
 
 def index(request):
     context = {
     }
     return render(request, 'home.html', context=context)
+
+def book_machine(request):
+    if request.method == 'POST':
+        form = FESEMForm(request.POST)
+        if form.is_valid():
+            return HttpResponse('Thanks')
+
+    else:
+        form = FESEMForm()
+
+    return render(request, 'booking_portal/fesem.html', {'form': form})
