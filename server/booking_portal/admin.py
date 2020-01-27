0716e255f0695bb6ee385fbc9dee179a604c7162
Ujjwal Raizada <ujjwalraizada@gmail.com>
2020-01-27 14:27:35 +0530

    add user models

--- server/booking_portal/admin.py
+++ server/booking_portal/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Student, Faculty, EmailModel, LabAssistant
 
 
 admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Student)
+admin.site.register(Faculty)
+admin.site.register(EmailModel)
+admin.site.register(LabAssistant)
+
+
--- server/booking_portal/models.py
+++ server/booking_portal/models.py
@@ -3,6 +3,42 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    first_name = None
+    last_name = None
+    email = models.EmailField("email address", unique=True, primary_key=True)
+    name = models.CharField(max_length=50)
 
-    
+    @property
+    def short_id(self):
+        return self.email[:self.email.find("@")].lower()
+
+    def __str__(self):
+        return f"{self.name} ({self.short_id})"
+
+
+class Student(CustomUser):
+    id_number = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "student"
+        default_related_name = "students"
+
+
+class Faculty(CustomUser):
+    class Meta:
+        verbose_name = "faculty"
+        verbose_name_plural = "faculties"
+        default_related_name = "faculties"
+
+
+class LabAssistant(CustomUser):
+    class Meta:
+        verbose_name = "lab assistant"
+        default_related_name = "labassistants"
+
+
+class EmailModel(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT)
+    text = models.CharField(max_length=500)
+    date_time = models.DateTimeField()
