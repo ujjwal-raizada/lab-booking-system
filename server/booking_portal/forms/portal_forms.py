8a47a912c1ed1c6e6ea6a4dae452945f697e3e6e
Pranjal Gupta <pranjalgupta2199@gmail.com>
2020-02-01 02:10:53 +0530

    django-views: Connects instrument-list, view-slot and book-machine 	* Adds Slot Form for displaying Available Slots 	* Renames Status Choice in Slot Class for easy filtering

--- server/booking_portal/forms/portal_forms.py
+++ server/booking_portal/forms/portal_forms.py
@@ -1,6 +1,13 @@
 from django import forms
-from booking_portal.models import Instrument
+from booking_portal.models import Instrument, Slot
 
 
 class IntrumentList(forms.Form):
     instruments = forms.ModelChoiceField(queryset=Instrument.objects.all())
+
+class SlotList(forms.Form):
+    def __init__(self, instr_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['slots'] = forms.ModelChoiceField(
+            queryset=Slot.objects.filter(instrument=instr_id,
+                                         status=Slot.STATUS_1))
--- server/booking_portal/urls.py
+++ server/booking_portal/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('book-machine/<int:id>', views.book_machine, name='book-machine'),
     path('email/', views.email, name='email'),
     path('instrument-list/', views.instrument_list, name='instrument-list'),
+    path('view-slots/', views.slot_list, name='slot-list'),
 ]
