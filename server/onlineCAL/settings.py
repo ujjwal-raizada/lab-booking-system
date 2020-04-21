27f1a5ddcde290fc627b1da9587a38722bd5da15
Daksh <dyashlaha039909@gmail.com>
2020-04-21 15:16:07 +0530

    Add Homepage for onlineCAL

--- server/onlineCAL/settings.py
+++ server/onlineCAL/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'onlineCAL',
     'booking_portal',
     'django.contrib.admin',
     'django.contrib.auth',
--- server/onlineCAL/urls.py
+++ server/onlineCAL/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
-    path('', include('booking_portal.urls')),
+    path('', views.index, name='home'),
     path('booking/', include('booking_portal.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
