53ac32faa6ba956b578302b60cc71f7881e0e1ee
Pranjal Gupta <pranjalgupta2199@gmail.com>
2020-04-22 01:48:59 +0530

    django-views: Adds 'view-application/<int:id>' endpoint 	* Modifies all the form html templates 	* Modifies template-form dictionary to contain the Form class 	* Adds Generic Relation in Request Objects 	* Fixes Request User matching in accept/reject views

--- server/booking_portal/config.py
+++ server/booking_portal/config.py
@@ -1,23 +1,27 @@
 from .forms import *
+from .models import *
 
 form_template_dict = {
-    1: ('booking_portal/fesem.html', FESEMForm),
-    2: ('booking_portal/tcspc.html', TCSPCForm),
-    3: ('booking_portal/ftir.html', FTIRForm),
-    4: ('booking_portal/lcms.html', LCMSForm),
-    5: ('booking_portal/rheometer.html', RheometerForm),
-    6: ('booking_portal/aas.html', AASForm),
-    7: ('booking_portal/tga.html', TGAForm),
-    8: ('booking_portal/bet.html', BETForm),
-    9: ('booking_portal/cdspectrophotometer.html', CDSpectrophotometerForm),
-    10: ('booking_portal/lscm.html', LSCMForm),
-    11: ('booking_portal/dsc.html', DSCForm),
-    12: ('booking_portal/gc.html', GCForm),
-    13: ('booking_portal/edxrf.html', EDXRFForm),
-    14: ('booking_portal/hplc.html', HPLCForm),
-    15: ('booking_portal/nmr.html', NMRForm),
-    16: ('booking_portal/pxrd.html', PXRDForm),
-    17: ('booking_portal/scxrd.html', SCXRDForm),
-    18: ('booking_portal/xps.html', XPSForm),
-    19: ('booking_portal/uvspectrophotometer.html', UVSpectrophotometerForm),
+    1: ('booking_portal/fesem.html', FESEMForm, FESEM),
+    2: ('booking_portal/tcspc.html', TCSPCForm, TCSPC),
+    3: ('booking_portal/ftir.html', FTIRForm, FTIR),
+    4: ('booking_portal/lcms.html', LCMSForm, LCMS),
+    5: ('booking_portal/rheometer.html', RheometerForm, Rheometer),
+    6: ('booking_portal/aas.html', AASForm, AAS),
+    7: ('booking_portal/tga.html', TGAForm, TGA),
+    8: ('booking_portal/bet.html', BETForm, BET),
+    9: ('booking_portal/cdspectrophotometer.html', CDSpectrophotometerForm, CDSpectrophotometer),
+    10: ('booking_portal/lscm.html', LSCMForm, LSCM),
+    11: ('booking_portal/dsc.html', DSCForm, DSC),
+    12: ('booking_portal/gc.html', GCForm, GC),
+    13: ('booking_portal/edxrf.html', EDXRFForm, EDXRF),
+    14: ('booking_portal/hplc.html', HPLCForm, HPLC),
+    15: ('booking_portal/nmr.html', NMRForm, HPLC),
+    16: ('booking_portal/pxrd.html', PXRDForm, PXRD),
+    17: ('booking_portal/scxrd.html', SCXRDForm, SCXRD),
+    18: ('booking_portal/xps.html', XPSForm, XPS),
+    19: ('booking_portal/uvspectrophotometer.html', UVSpectrophotometerForm, UVSpectrophotometer),
 }
+
+view_application_dict = {model: (template, form)
+                         for idx, (template, form, model) in form_template_dict.items()}
--- server/booking_portal/views.py
+++ server/booking_portal/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 
-from .config import form_template_dict
+from .config import form_template_dict, view_application_dict
 from .forms.portal_forms import IntrumentList, SlotList
 from .models import EmailModel, Instrument, Slot, Request, Student, Faculty, LabAssistant
 from .permissions import is_faculty, is_lab_assistant, is_student
@@ -43,9 +43,10 @@ def email(request):
 @login_required
 @user_passes_test(is_student)
 def book_machine(request, id):
-    template, form = form_template_dict.get(id)
+    template, form, _ = form_template_dict.get(id)
     if request.method == 'GET':
-        return render(request, template, {'form': form(initial={'user_name': Student.objects.filter(username=request.user.username).first().id})})
+        return render(request, template, {'form': form(initial={'user_name': Student.objects.filter(username=request.user.username).first().id}),
+                                                                'edit' : True})
 
     elif request.method == "POST" and form(request.POST).is_valid():
         slot_id = request.GET['slots']
@@ -61,20 +62,21 @@ def book_machine(request, id):
 
         # TODO: Object Lock on Request Object
         if slot_instance and student_instance and faculty_instance and instr_instance:
+            model_object = form(request.POST).save()
             req_instance = Request(student=student_instance,
                                     faculty=faculty_instance,
                                     instrument=instr_instance,
                                     slot=slot_instance,
-                                    status=Request.STATUS_1)
+                                    status=Request.STATUS_1,
+                                    content_object=model_object)
             req_instance.save()
-            form(request.POST).save()
             messages.success(request, 'Form Submission Successful')
             return HttpResponseRedirect('/booking/')
         else:
-            return render(request, template, {'form': form(request.POST)})
+            return render(request, template, {'form': form(request.POST), 'edit' : True})
 
     else:
-        return render(request, template, {'form': form(request.POST)})
+        return render(request, template, {'form': form(request.POST), 'edit' : True})
 
 @login_required
 @user_passes_test(is_student)
@@ -89,7 +91,6 @@ def slot_list(request):
 @user_passes_test(is_faculty)
 def faculty_portal(request):
     requests_objects = Request.objects.filter(faculty=request.user, status=Request.STATUS_1)
-    print(requests_objects)
     return render(request, 'booking_portal/portal_forms/faculty_portal.html',
                   {'requests': requests_objects, 'usertype': 'faculty'})
 
@@ -102,7 +103,7 @@ def faculty_request_accept(request, id):
     except:
         raise Http404()
     faculty = request_object.faculty
-    if (faculty == request.user):
+    if (faculty == Faculty.objects.get(id=request.user.id)):
         request_object.status = Request.STATUS_2
         request_object.message = "accept"
         request_object.lab_assistant = random.choice(LabAssistant.objects.all())
@@ -120,7 +121,7 @@ def faculty_request_reject(request, id):
     except:
         raise Http404()
     faculty = request_object.faculty
-    if faculty == request.user:
+    if (faculty == Faculty.objects.get(id=request.user.id)):
         request_object.status = Request.STATUS_2
         request_object.message = "reject"
         request_object.save()
@@ -143,7 +144,7 @@ def lab_assistant_accept(request, id):
     except:
         raise Http404()
     lab_assistant = request_object.lab_assistant
-    if (lab_assistant == request.user):
+    if (lab_assistant == LabAssistant.objects.get(id=request.user.id)):
         request_object.status = Request.STATUS_3
         request_object.message = "accept"
         request_object.save()
@@ -159,7 +160,7 @@ def lab_assistant_reject(request, id):
     except:
         raise Http404()
     lab_assistant = request_object.lab_assistant
-    if (lab_assistant == request.user):
+    if (lab_assistant == LabAssistant.objects.get(id=request.user.id)):
         request_object.status = Request.STATUS_3
         request_object.message = "reject"
         request_object.save()
@@ -173,3 +174,25 @@ def student_portal(request):
     request_objects = Request.objects.filter(student=request.user)
     return render(request, 'booking_portal/portal_forms/student_portal.html',
                   {'requests': request_objects, 'usertype': 'student'})
+
+@login_required
+def show_application(request, id):
+    try:
+        request_obj = Request.objects.get(id=id)
+    except:
+        raise Http404()
+    content_object = request_obj.content_object
+    template, form = view_application_dict[content_object._meta.model]
+
+    data = content_object.__dict__
+    data['user_name'] = Student.objects.get(id=data['user_name_id'])
+    data['sup_name'] = Faculty.objects.get(id=data['sup_name_id'])
+    form_object = form(data)
+
+    for field_val, val in data.items():
+        try:
+            form_object.fields[field_val].widget.attrs['readonly'] = True
+        except KeyError:
+            pass
+
+    return render(request, template, {'form': form_object, 'edit' : False})
