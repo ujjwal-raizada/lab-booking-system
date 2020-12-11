d7552f6300b8fbead3618c70c5561c118bdf9e70
Pranjal Gupta <pranjalgupta2199@gmail.com>
2020-12-12 00:44:23 +0530

    django-db: Adds atomic transactions for applications 	* Will prevent concurrent updates on Time Slots 	* Will prevent concurrent accept/reject requests of applications

--- server/booking_portal/views/instrument.py
+++ server/booking_portal/views/instrument.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
-
+from django.db import transaction, DatabaseError
 
 from ..forms.portal_forms import IntrumentList
 from ..config import form_template_dict
@@ -39,27 +39,41 @@ def book_machine(request, id):
         student_id = request.POST['user_name']
         faculty_id = request.POST['sup_name']
 
-        instr_instance = Instrument.objects.filter(id=id).first()
-        slot_instance = Slot.objects.filter(id=slot_id,
-                                            status=Slot.STATUS_1,
-                                            instrument=instr_instance).first()
-        student_instance = Student.objects.filter(id=student_id).first()
-        faculty_instance = Faculty.objects.filter(id=faculty_id).first()
+        try:
+            with transaction.atomic():
+                instr_instance = Instrument.objects.filter(id=id).first()
+                slot_instance = Slot.objects.select_for_update(nowait=True).filter(
+                                                        id=slot_id,
+                                                        status=Slot.STATUS_1,
+                                                        instrument=instr_instance).first()
+                student_instance = Student.objects.filter(id=student_id).first()
+                faculty_instance = Faculty.objects.filter(id=faculty_id).first()
 
-        # TODO: Object Lock on Request Object
-        if slot_instance and student_instance and faculty_instance and instr_instance:
-            model_object = form(request.POST).save()
-            req_instance = Request(student=student_instance,
-                                    faculty=faculty_instance,
-                                    instrument=instr_instance,
-                                    slot=slot_instance,
-                                    status=Request.STATUS_1,
-                                    content_object=model_object)
-            req_instance.save()
-            messages.success(request, 'Form Submission Successful')
+                if Request.objects.filter(instrument=instr_instance,
+                                          student=student_instance).exists():
+
+                    messages.error(request, "You already have an ongoing application for this machine")
+                    return HttpResponseRedirect("/booking/")
+
+                if slot_instance and student_instance and faculty_instance and instr_instance:
+                    model_object = form(request.POST).save()
+                    req_instance = Request(student=student_instance,
+                                            faculty=faculty_instance,
+                                            instrument=instr_instance,
+                                            slot=slot_instance,
+                                            status=Request.STATUS_1,
+                                            content_object=model_object)
+                    req_instance.save()
+                    messages.success(request, 'Form Submission Successful')
+                    return HttpResponseRedirect('/booking/')
+
+                elif not slot_instance:
+                    messages.error(request, "Sorry, This slot is not available anymore.")
+                    return HttpResponseRedirect('/booking/')
+
+        except DatabaseError:
+            messages.error(request, "Could not proccess your request, please try again.")
             return HttpResponseRedirect('/booking/')
-        else:
-            return render(request, template, {'form': form(request.POST), 'edit' : True})
 
     else:
         return render(request, template, {'form': form(request.POST), 'edit' : True})
--- server/manage.py
+++ server/manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from onlineCAL.config import email_username, email_password
 
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlineCAL.settings')
     try:
+        assert '' not in (email_username, email_password)
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -14,6 +16,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    except AssertionError as err:
+        raise Exception(
+            "Email username and password empty! " +
+            "See onlineCAL/config.py for more info"
+        ) from err
     execute_from_command_line(sys.argv)
 
 
