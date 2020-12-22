d46e90ca84bba41034aeb84f11d0d9f0a1e5c997
Daksh <dyashlaha039909@gmail.com>
2020-12-22 07:09:06 +0530

    django-view: Update Styling in navbar and Instrument and Slot lists

--- server/booking_portal/urls.py
+++ server/booking_portal/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
          views.show_application, name='view-application'),
     path('application/edit/remarks/<int:id>',
          views.add_remarks, name='add_remarks'),
-    path('change-password/', views.change_password, name="change_password")     
+    path('change-password/', views.change_password, name="change_password"),
+    path('admin-portal/', views.admin_portal, name='admin_portal')
 ]
