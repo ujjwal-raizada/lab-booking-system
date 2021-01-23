1754e29801f2062252ba4d023730e13bada50198
Abhijeet Viswa <abhijeetviswa@gmail.com>
2021-01-23 23:13:20 +0530

    add: STATIC_ROOT setting
    This is the location static files will be collected if the `collectstatic`
    management command is used

--- server/onlineCAL/settings.py
+++ server/onlineCAL/settings.py
@@ -14,7 +14,7 @@ import os
 from .config import email_password, email_username
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
