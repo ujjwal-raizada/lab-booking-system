e261a764147c0f24722439365030b7a85bb71c6d
Abhijeet Viswa <abhijeetviswa@gmail.com>
2021-01-30 22:30:58 +0530

    ux: Feedback messages when adding bulk slots

--- server/booking_portal/admin/slot.py
+++ server/booking_portal/admin/slot.py
@@ -1,15 +1,14 @@
-import csv
 import datetime
-from io import TextIOWrapper
 
-from django.urls import path
 from django.contrib import admin
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.urls import path
 from django.utils.translation import gettext_lazy
 from rangefilter.filter import DateRangeFilter
 
-from ..models import Instrument, Slot
 from ..forms.adminForms import BulkTimeSlotForm
+from ..models import Instrument, Slot
 
 
 class SlotFilterByInstrument(admin.SimpleListFilter):
@@ -88,7 +87,6 @@ class SlotAdmin(admin.ModelAdmin):
         }
 
         if request.method == 'POST':
-
             ## get the queryset for instruments
             try:
                 instr = Instrument.objects.filter(
@@ -96,10 +94,9 @@ class SlotAdmin(admin.ModelAdmin):
             except ValueError:
                 instr = Instrument.objects.all()
 
-
-            ## preproces the form fields to desired objects
+            ## preprocess the form fields to desired objects
             ## `today` the starting day for slot creation
-            today = datetime.datetime.strptime(
+            start_date = datetime.datetime.strptime(
                 request.POST.get('date'), '%Y-%m-%d')
             start_time = int(request.POST.get('start_time').split(':')[0])
             end_time = int(request.POST.get('end_time').split(':')[0])
@@ -110,16 +107,19 @@ class SlotAdmin(admin.ModelAdmin):
 
             ## handle exceptional cases and return to previous page
             if start_time >= end_time:
-                return redirect('..')
-            if duration == None:
-                return redirect('..')
-            if today.date() < datetime.date.today():
-                return redirect('..')
+                messages.error(request, "Start time cannot be greater than end time!")
+                return redirect('.')
+            elif duration == None:
+                messages.error(request, "Duration cannot be empty!")
+                return redirect('.')
+            elif start_date.date() < datetime.date.today():
+                messages.error(request, "Start date cannot be before today" )
+                return redirect('.')
 
             # get the next `delta` days after `today`
-            today_weekday = today.weekday()
+            today_weekday = start_date.weekday()
             next_days = [
-                today + datetime.timedelta(days=var) for var in range(0, delta)]
+                start_date + datetime.timedelta(days=var) for var in range(0, delta)]
 
             ## generate datetime objects for the next `delta` days
             all_slots = {}
@@ -154,11 +154,10 @@ class SlotAdmin(admin.ModelAdmin):
                                 status=Slot.STATUS_1,
                                 date=day,
                                 time=time_slot.time())
-
                             slot_obj.save()
 
+            messages.success(request, "Slots successfully created!")
             return redirect("..")
-
         else:
             form = BulkTimeSlotForm()
             payload = {"form": form}
