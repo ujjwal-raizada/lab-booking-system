04ef9828d4f0c07eea5c7f7af2e11c8a54b028cb
Abhijeet Viswa <abhijeetviswa@gmail.com>
2021-02-07 02:17:26 +0530

    cleanup: Clean up all the auth templates

--- server/onlineCAL/forms.py
+++ server/onlineCAL/forms.py
@@ -1,6 +1,16 @@
-from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
-from django.contrib.auth.password_validation import password_validators_help_texts
-from django.template.loader import render_to_string
+from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm,
+                                       SetPasswordForm)
+
+
+class CustomLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].label = ''
+        self.fields['username'].widget.attrs['placeholder'] = 'Email ID'
+
+        self.fields['password'].label = ''
+        self.fields['password'].widget.attrs['placeholder'] = 'Password'
 
 
 class CustomPasswordResetForm(PasswordResetForm):
@@ -15,17 +25,8 @@ class CustomSetPasswordForm(SetPasswordForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        context = {
-            'help_texts': password_validators_help_texts(),
-        }
-        help_text = render_to_string(
-            'registration/password_validation_help_text.html',
-            context,
-        )
-
         self.fields['new_password1'].label = ''
-        self.fields['new_password1'].widget.attrs['placeholder'] = 'Enter Password'
-        self.fields['new_password1'].help_text = help_text
+        self.fields['new_password1'].widget.attrs['placeholder'] = 'New Password'
 
         self.fields['new_password2'].label = ''
         self.fields['new_password2'].widget.attrs['placeholder'] = 'Confirm Password'
--- server/onlineCAL/urls.py
+++ server/onlineCAL/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
+from django.contrib.auth.views import (LoginView, PasswordResetConfirmView,
+                                       PasswordResetView)
 from django.urls import include, path
 
-from .forms import CustomPasswordResetForm, CustomSetPasswordForm
+from .forms import (CustomLoginForm, CustomPasswordResetForm,
+                    CustomSetPasswordForm)
 
 urlpatterns = [
     path('', include('booking_portal.urls')),
     # path('booking/', include('booking_portal.urls')),
     path('admin/', admin.site.urls),
+    path('auth/login/', LoginView.as_view(form_class=CustomLoginForm)),
     path('auth/password_reset/', PasswordResetView.as_view(form_class=CustomPasswordResetForm)),
     path('auth/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(form_class=CustomSetPasswordForm)),
     path('auth/', include('django.contrib.auth.urls')),
