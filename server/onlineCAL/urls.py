8260db0947290978e8f5890abfc3b39e01bad812
Abhijeet Viswa <abhijeetviswa@gmail.com>
2021-02-08 23:28:00 +0530

    add: Templates for password reset email

--- server/onlineCAL/urls.py
+++ server/onlineCAL/urls.py
@@ -11,7 +11,12 @@ urlpatterns = [
     # path('booking/', include('booking_portal.urls')),
     path('admin/', admin.site.urls),
     path('auth/login/', LoginView.as_view(form_class=CustomLoginForm)),
-    path('auth/password_reset/', PasswordResetView.as_view(form_class=CustomPasswordResetForm)),
+    path('auth/password_reset/', PasswordResetView.as_view(
+        form_class=CustomPasswordResetForm,
+        email_template_name='email/registration/password_reset_email.txt',
+        html_email_template_name='email/registration/password_reset_email.html',
+        subject_template_name='email/registration/password_reset_subject.txt'
+    )),
     path('auth/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(form_class=CustomSetPasswordForm)),
     path('auth/', include('django.contrib.auth.urls')),
 ]
