00aa8785d2a2394e6e5f681dade10b8f630407a8
Abhijeet Viswa <abhijeetviswa@gmail.com>
2021-02-14 23:11:07 +0530

    fix: Backup script
    Make the paths more portable

--- database-backup.py
+++ database-backup.py
@@ -1,14 +1,18 @@
 import pathlib
 import datetime
+import os
 from shutil import copyfile
 
 # This script will be called by pythonanywhere tasks handler (or any other cron job)
 # once a day.
 
+cwd = os.path.abspath(os.path.dirname(__file__))
+os.chdir(cwd)
+
 backup_dir = pathlib.Path('backups')
 backup_dir.mkdir(parents=True, exist_ok=True)
 
-date = datetime.datetime.now().strftime("%d-%m-%Y")
+date = datetime.datetime.now().strftime("%Y-%m-%d")
 
 source = pathlib.Path('server/db.sqlite3')
 dest = pathlib.Path('backups/db_backup_' + date)
