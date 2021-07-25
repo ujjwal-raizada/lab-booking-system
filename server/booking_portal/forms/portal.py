4357095d8468400d8856f53b1a62ef7f1be3f113
Abhijeet Viswa <abhijeetviswa@gmail.com>
2021-07-26 00:56:33 +0530

    ui: Crispify instrument booking forms

--- server/booking_portal/forms/portal.py
+++ server/booking_portal/forms/portal.py
@@ -1,4 +1,7 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, ButtonHolder, Submit
 from django import forms
+from django.urls import reverse
 from django.utils.timezone import now
 
 from ..models.instrument import Instrument
@@ -18,6 +21,19 @@ class InstrumentList(forms.Form):
     """Form for selecting instruments for booking"""
     instruments = forms.ModelChoiceField(queryset=Instrument.objects.all())
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.form_show_labels = False
+        self.helper.form_action = reverse('slot-list')
+        self.helper.layout = Layout(
+            'instruments',
+            ButtonHolder(
+                Submit('proceed', value="Proceed", css_class='btn-primary btn-md')
+            )
+        )
+
 
 class SlotList(forms.Form):
     """Form for selecting an empty slot of a given
@@ -30,4 +46,15 @@ class SlotList(forms.Form):
                 status=Slot.STATUS_1,
                 date__gte=now().date()
             )
-)
+        )
+
+        self.helper = FormHelper(self)
+        self.helper.form_show_labels = False
+        self.helper.form_action = reverse('book-machine', kwargs={'instr_id': instr.pk})
+        self.helper.form_method = 'GET'
+        self.helper.layout = Layout(
+            'slots',
+            ButtonHolder(
+                Submit('proceed', value="Proceed", css_class='btn-primary btn-md')
+            )
+        )
