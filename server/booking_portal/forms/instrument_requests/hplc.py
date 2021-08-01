abhijeetviswa/onlineCAL

Cleanup: Package tree and module names
* Removed redundant 'form' prefix to modules which contain forms.
* Reorganized forms for a more sane directory structure.

server/booking_portal/forms/instrument_requests/hplc.py
@@ -1,10 +1,11 @@
 from django import forms
 
-from ..models.instrument.form_models import HPLC
-from .userform import UserDetailsForm, UserRemarkForm
+from booking_portal.models.instrument.requests import HPLC
+
+from .base import Form, UserRemarkForm
 
 
-class HPLCForm(UserDetailsForm, UserRemarkForm):
+class HPLCForm(Form, UserRemarkForm):
     title = "HPLC-Shimadzu"
     subtitle = "HPLC-Shimadzu UFLC"
     help_text = '''

ujjwal-raizada/lab-booking-system

server/booking_portal/forms/instrument_requests/hplc.py
@@ -1,7 +1,8 @@
 from django import forms
 
-from ..models.instrument.form_models import HPLC
-from .userform import UserDetailsForm, UserRemarkForm
+from booking_portal.models.instrument.requests import HPLC
+
+from .base import UserDetailsForm, UserRemarkForm
 
 
 class HPLCForm(UserDetailsForm, UserRemarkForm):