a81089c7929ee53955d3fdfa8fe8fddd8c2b3425
Abhijeet Viswa <abhijeetviswa@gmail.com>
2021-09-28 10:11:30 +0530

    Change: `send_email` command to run in a loop

--- server/booking_portal/management/commands/send_emails.py
+++ server/booking_portal/management/commands/send_emails.py
@@ -1,27 +1,32 @@
-from smtplib import SMTPException
-from django.core.management.base import BaseCommand
+from time import sleep
 from django.conf import settings
 from django.core.mail import send_mass_mail
+from django.core.management.base import BaseCommand
+
 from ...models.email import EmailModel
 
 MAX_EMAIL_PER_COMMAND = 30
-
+SLEEP_PER_LOOP = 60
 
 class Command(BaseCommand):
-    help = 'Send all queued emails'
+    help = 'Always running management command to send all queued emails'
 
     def handle(self, *args, **options):
-        emails = EmailModel.objects.filter(sent=False).order_by('date_time')[:MAX_EMAIL_PER_COMMAND]
-        datatuple = []
-        for email in emails:
-            message = (
-                email.subject,
-                email.text,
-                settings.EMAIL_HOST_USER,
-                [email.receiver],
-            )
-            datatuple.append(message)
-            email.sent = True
-        send_mass_mail(datatuple, fail_silently=False)
+        while True:
+            emails = EmailModel.objects.filter(sent=False).order_by('date_time')[:MAX_EMAIL_PER_COMMAND]
+            datatuple = []
+            for email in emails:
+                message = (
+                    email.subject,
+                    email.text,
+                    settings.EMAIL_HOST_USER,
+                    [email.receiver],
+                )
+                datatuple.append(message)
+                email.sent = True
+            send_mass_mail(datatuple, fail_silently=False)
 
-        EmailModel.objects.bulk_update(emails, ['sent'])
+            EmailModel.objects.bulk_update(emails, ['sent'])
+            sleep(SLEEP_PER_LOOP)
+
+
