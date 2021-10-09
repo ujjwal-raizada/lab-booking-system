e810864ae7b6299b118bcb3e7001437ebc807bb6
Abhijeet Viswa <abhijeetviswa@gmail.com>
2021-10-09 12:49:16 +0530

    Change: Precision of UTM 'Test Speed' to 0.0001

--- server/booking_portal/models/instrument/requests.py
+++ server/booking_portal/models/instrument/requests.py
@@ -555,9 +555,9 @@ class UTM(UserDetail, UserRemark):
 
     material = models.CharField(max_length=75)
     test_type = models.CharField(max_length=75, choices=TEST_TYPE_CHOICES)
-    test_speed = models.DecimalField(max_digits=7, decimal_places=2, help_text=(
+    test_speed = models.DecimalField(max_digits=9, decimal_places=4, help_text=(
         "<small>"
-        "Precision upto 2 decimal places"
+        "Precision upto 4 decimal places"
         "</small>"
     ))
     temperature = models.IntegerField(help_text=(
